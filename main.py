6e302fed489bc0afe3c5fb243749ca75af8acc8c
Cole Mickens <cole.mickens@gmail.com>
2021-09-05 23:29:16 +0000

    convert to basic python

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+def make_chocolate(small, big, goal):
+    big = big * 5
+    sum = big + small
+
+    return -1
+
+
+if __name__ == '__main__':
+    # check
+    print(make_chocolate(4, 1, 9))
+    # make_chocolate(4,1,10) = -1
+    # make_chocolate(4,1,7) = -2
